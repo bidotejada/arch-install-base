ae0cf896226d5dce13d7aeb7d5060a2e1df2565e
bidotejada <45131500+bidotejada@users.noreply.github.com>
2022-04-14 13:15:08 -0400

    Update base.sh

--- base.sh
+++ base.sh
@@ -14,7 +14,7 @@ pacman -Syy
 
 pacman -S --needed --noconfirm xorg xorg-server amd-ucode plasma plasma-desktop sddm konsole kwrite dolphin xf86-video-amdgpu mesa vulkan-radeon libva-mesa-driver mesa-vdpau xdg-user-dirs xdg-utils wpa_supplicant alsa alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils ufw openssh git bash-completion rust go base-devel linux-headers bat lsd grub-btrfs grub-customizer mlocate tldr nmap ranger neovim dosfstools nfs-utils gvfs gvfs-smb inetutils cups hplip reflector acpi acpid lm_sensors acpi_call tlp flatpak sof-firmware nss-mdns os-prober ntfs-3g avahi sudo nano vlc btrfs-progs
 
-pacman -S --needed --noconfirm papirus-icon-theme materia-kde htop neofetch
+pacman -S --needed --noconfirm papirus-icon-theme materia-kde htop neofetch discord
 
 updatedb
 tldr -u

0413ce081ff94bbfa61a1576b78b5edb9795c371
bidotejada <45131500+bidotejada@users.noreply.github.com>
2022-04-14 19:14:44 -0400

    Update base.sh

--- base.sh
+++ base.sh
@@ -12,15 +12,25 @@ pacman -S --needed --noconfirm reflector
 reflector -c us -a 3 --sort rate -p http --save /etc/pacman.d/mirrorlist --verbose
 pacman -Syy
 
+# General Utilities
 pacman -S --needed --noconfirm xorg xorg-server amd-ucode xdg-user-dirs xdg-utils wpa_supplicant alsa alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils ufw openssh git bash-completion rust go base-devel linux-headers bat lsd grub-btrfs grub-customizer mlocate tldr nmap ranger neovim dosfstools nfs-utils gvfs gvfs-smb inetutils cups hplip reflector acpi acpid lm_sensors acpi_call tlp flatpak sof-firmware nss-mdns os-prober ntfs-3g avahi sudo nano vlc btrfs-progs papirus-icon-theme htop neofetch discord
+# Plasma Desktop
 pacman -S --needed --noconfirm plasma plasma-desktop sddm konsole kwrite dolphin materia-kde 
+# Gnome Desktop
+#pacman -S --needed --noconfirm
+# Budgie Desktop
+#pacman -S --needed --noconfirm
+# Cinnamon Desktop
+#pacman -S --needed --noconfirm
+# xfce Desktop
+#pacman -S --needed --noconfirm
 
 updatedb
 tldr -u
 
 # GPU Drivers
 pacman -S --noconfirm xf86-video-amdgpu mesa vulkan-radeon libva-mesa-driver mesa-vdpau
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings nvidia-dkms
 
 # mkinitcpio -P
 grub-mkconfig -o /boot/grub/grub.cfg

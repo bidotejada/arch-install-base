2bd12f529e35a7703197e55e69ac988886f4eadb
bidotejada <45131500+bidotejada@users.noreply.github.com>
2022-04-14 18:57:04 -0400

    sectioning parts of script to make it make sense

--- base.sh
+++ base.sh
@@ -7,24 +7,25 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 timedatectl set-ntp true
 hwclock --systohc
 
-pacman -Syy
+# pacman -Syy
 pacman -S --needed --noconfirm reflector
 reflector -c us -a 3 --sort rate -p http --save /etc/pacman.d/mirrorlist --verbose
 pacman -Syy
 
-pacman -S --needed --noconfirm xorg xorg-server amd-ucode plasma plasma-desktop sddm konsole kwrite dolphin xf86-video-amdgpu mesa vulkan-radeon libva-mesa-driver mesa-vdpau xdg-user-dirs xdg-utils wpa_supplicant alsa alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils ufw openssh git bash-completion rust go base-devel linux-headers bat lsd grub-btrfs grub-customizer mlocate tldr nmap ranger neovim dosfstools nfs-utils gvfs gvfs-smb inetutils cups hplip reflector acpi acpid lm_sensors acpi_call tlp flatpak sof-firmware nss-mdns os-prober ntfs-3g avahi sudo nano vlc btrfs-progs
-
-pacman -S --needed --noconfirm papirus-icon-theme materia-kde htop neofetch discord
+pacman -S --needed --noconfirm xorg xorg-server amd-ucode xdg-user-dirs xdg-utils wpa_supplicant alsa alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils ufw openssh git bash-completion rust go base-devel linux-headers bat lsd grub-btrfs grub-customizer mlocate tldr nmap ranger neovim dosfstools nfs-utils gvfs gvfs-smb inetutils cups hplip reflector acpi acpid lm_sensors acpi_call tlp flatpak sof-firmware nss-mdns os-prober ntfs-3g avahi sudo nano vlc btrfs-progs papirus-icon-theme htop neofetch discord
+pacman -S --needed --noconfirm plasma plasma-desktop sddm konsole kwrite dolphin materia-kde 
 
 updatedb
 tldr -u
 
-# pacman -S --noconfirm xf86-video-amdgpu
+# GPU Drivers
+pacman -S --noconfirm xf86-video-amdgpu mesa vulkan-radeon libva-mesa-driver mesa-vdpau
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 # mkinitcpio -P
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# Enabling Services
 systemctl enable NetworkManager
 systemctl enable cups.service
 systemctl enable sddm
@@ -37,12 +38,13 @@ systemctl enable reflector.timer
 systemctl enable ufw
 systemctl enable acpid
 
+# Create User
 useradd -m willy
 echo willy:password | chpasswd
 # usermod -aG <group> willy
-
 echo "willy ALL=(ALL) ALL" >> /etc/sudoers.d/willy
 
+# Enable Firewall and Allow External SSH connections
 ufw enable
 ufw allow proto tcp from any to any port 22 comment "SSH"
 
